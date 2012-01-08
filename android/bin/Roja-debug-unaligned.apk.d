050c6fdd5d463eb48bcde4dcba33aca58a4a7dc5
Maciej Płaza <plaza.maciej@gmail.com>
2012-01-08 12:22:15 +0100

    Clean Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

--- android/bin/Roja-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/kermit/Projekty/ROJA/android/bin/Roja-debug-unaligned.apk : \
-/home/kermit/Projekty/ROJA/android/bin/Roja.ap_ \
-/home/kermit/Projekty/ROJA/android/bin/classes.dex \
-/home/kermit/Projekty/ROJA/android/libs/armeabi/libroja.so \
-/home/kermit/Projekty/ROJA/android/libs/armeabi/gdbserver \
